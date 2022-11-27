3f17ba5368a368ef5bc9e06ebede4070dca86eaf
IfiZa <108893104+IfiZa@users.noreply.github.com>
2022-11-27 17:33:01 +0100

    Update forecast component

--- src/Forecast.js
+++ src/Forecast.js
@@ -8,6 +8,58 @@ export default function Forecast() {
       <h3>Next 6 days</h3>
       <div className="weather-forecast">
         <div className="icon-forecast"></div>
+        <div class="col">
+          <div class="card text-center border-light mb-3 h-100 opacity-85">
+            <div class="card-body">
+              <h5 class="card-next-day">Sun 07/08</h5>
+              <img
+                src="images\icons\cloud.png"
+                class="card-img-top"
+                alt="Cloudy"
+              />
+              <p class="card-next-day-MinMax">15°C / 24°C</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card text-center border-light mb-3 h-100 opacity-85">
+            <div class="card-body">
+              <h5 class="card-next-day">Mon 08/08</h5>
+              <img
+                src="images\icons\clear-sky.png"
+                class="card-img-top"
+                alt="Clear Sky"
+              />
+              <p class="card-next-day-MinMax">15°C / 25°C</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card text-center border-light mb-3 h-100 opacity-85">
+            <div class="card-body">
+              <h5 class="card-next-day">Tue 09/08</h5>
+              <img
+                src="images\icons\sunny.png"
+                class="card-img-top"
+                alt="Sunny"
+              />
+              <p class="card-next-day-MinMax">18°C / 30°C</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card text-center border-light mb-3 h-100 opacity-85">
+            <div class="card-body">
+              <h5 class="card-next-day">Wed 10/08</h5>
+              <img
+                src="images\icons\sunny.png"
+                class="card-img-top"
+                alt="Sunny"
+              />
+              <p class="card-next-day-MinMax">18°C / 30°C</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
