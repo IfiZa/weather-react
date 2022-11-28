103c67f2f57c4246e24996b540aa46b37434570f
IfiZa <108893104+IfiZa@users.noreply.github.com>
2022-11-28 20:39:59 +0100

    Update HostingInfo component and its css properties

--- src/HostingInfo.js
+++ src/HostingInfo.js
@@ -4,28 +4,26 @@ import "./HostingInfo.css";
 
 export default function HostingInfo() {
   return (
-    <div className="HostingInfo">
-      <small className="hosting">
-        <a
-          href="https://github.com/IfiZa/weather-react"
-          alt="Github repository link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open source code{" "}
-        </a>
-        by IfiZamb <br />
-        Doodle vector by{" "}
-        <a
-          href="https://www.freepik.com/author/rawpixel-com"
-          target="_blank"
-          alt="Author link"
-          className="vector-source"
-          rel="noreferrer"
-        >
-          Rawpixel
-        </a>
-      </small>
+    <div className="hostingInfo">
+      <a
+        href="https://github.com/IfiZa/weather-react"
+        alt="Github repository link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub open source code{" "}
+      </a>
+      by IfiZamb <br />
+      Doodle vector by{" "}
+      <a
+        href="https://www.freepik.com/author/rawpixel-com"
+        target="_blank"
+        alt="Author link"
+        className="vector-source"
+        rel="noreferrer"
+      >
+        Rawpixel
+      </a>
     </div>
   );
 }
