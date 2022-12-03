7d65db0f6bb0b5f78ba516304ec1cb36196723ae
IfiZa <108893104+IfiZa@users.noreply.github.com>
2022-12-03 20:48:33 +0100

    Update SearchCity.js

--- src/SearchCity.js
+++ src/SearchCity.js
@@ -6,22 +6,20 @@ export default function SearchCity() {
   return (
     <div className="SearchCity">
       <div className="card p-3">
-        <div className="card-body p-0">
-          <form id="search-form">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Type a city name"
-              autoComplete="off"
-            />
-            <button type="submit" className="btn btn-light p-1 mt-2">
-              Search
-            </button>
-            <button type="submit" className="btn btn-light p-1 mt-2">
-              Current
-            </button>
-          </form>
-        </div>
+        <form id="search-form">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Type a city name"
+            autoComplete="off"
+          />
+          <button type="submit" className="btn btn-light p-1 mt-2">
+            Search
+          </button>
+          <button type="submit" className="btn btn-light p-1 mt-2">
+            Current
+          </button>
+        </form>
       </div>
     </div>
   );
