4ac23244d90bc307d17f1205728e87feb4f8916a
IfiZa <108893104+IfiZa@users.noreply.github.com>
2022-12-03 20:47:41 +0100

    Removed unnecessary bootstrap property

--- src/ExtraData.js
+++ src/ExtraData.js
@@ -6,38 +6,36 @@ export default function ExtraData(props) {
   return (
     <div className="ExtraData">
       <div className="card p-3 mb-2 ">
-        <div className="card-body p-0 text-center  ">
-          <div className="row row-cols-2 row-cols-md-3 g-3">
-            <div className="col">
-              Real Feel
-              <br />
-              <span className="real-feel">{props.data.realFeel} °C</span>
-            </div>
-            <div className="col">
-              Humidity
-              <br />
-              <span className="humidity">{props.data.humidity} %</span>
-            </div>
-            <div className="col">
-              Wind
-              <br />
-              <span className="wind">{props.data.wind} m/s</span>
-            </div>
-            <div className="col">
-              Pressure
-              <br />
-              <span className="pressure">{props.data.pressure} hPa</span>
-            </div>
-            <div className="col">
-              Sunrise
-              <br />
-              <span className="sunrise">06:10</span>
-            </div>
-            <div className="col">
-              Sunset
-              <br />
-              <span className="sunset">20:59</span>
-            </div>
+        <div className="row row-cols-2 row-cols-md-3 g-3">
+          <div className="col">
+            Real Feel
+            <br />
+            <span className="real-feel">{props.data.realFeel} °C</span>
+          </div>
+          <div className="col">
+            Humidity
+            <br />
+            <span className="humidity">{props.data.humidity} %</span>
+          </div>
+          <div className="col">
+            Wind
+            <br />
+            <span className="wind">{props.data.wind} m/s</span>
+          </div>
+          <div className="col">
+            Pressure
+            <br />
+            <span className="pressure">{props.data.pressure} hPa</span>
+          </div>
+          <div className="col">
+            Sunrise
+            <br />
+            <span className="sunrise">06:10</span>
+          </div>
+          <div className="col">
+            Sunset
+            <br />
+            <span className="sunset">20:59</span>
           </div>
         </div>
       </div>
